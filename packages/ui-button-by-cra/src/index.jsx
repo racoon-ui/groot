41798a6d1ccbfb6216f5695772de5a25f9cd39f3
ilhwan.ryu <ih.ryu.dev@gmail.com>
2019-06-13 19:50:48 +0900

    ui-button

---
+++ packages/ui-button-by-cra/src/index.jsx
@@ -0,0 +1,6 @@
+// Hello World
+import React from 'react';
+import { Button } from '@rmwc/button';
+import '@material/button/dist/mdc.button.css';
+
+export const RCButton = props => <Button style={{color:'tomato'}}>Hello World</Button>;
